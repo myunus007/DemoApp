1531b0e1d48fcbdc57a7fbc241cf5ce30781f00b
mohammad <csit.yunus23@gmail.com>
2018-08-08 15:06:43 +0530

    Initial Project Setup
    Created new project.

---
+++ iOSTest/iOSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Mohammad Yunus on 08/08/18.
+//  Copyright © 2018 Mohammad Yunus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
